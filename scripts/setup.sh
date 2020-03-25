5e4913971c2d9c15d2cd8c8ef825dc2be1acb4cb
Anselmo Cervera <acervera@fnal.gov>
2020-03-25 14:04:44 +0100

    correct bug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,8 @@ if [[ ${HOSTNAME} =~ "neutrinos" ]] ; then
    export ROOTSYS=/data4/NEXT/software/scisoft/root/v6_18_04/Linux64bit+2.6-2.12-e17-prof
    export PATH=/data4/NEXT/software/scisoft/cmake/v3_14_3/Linux64bit+2.6-2.12/bin:/data4/NEXT/software/scisoft/gcc/v7_3_0/Linux64bit+2.6-2.12/bin:$PATH
    export LD_LIBRARY_PATH=/data4/NEXT/software/scisoft/tbb/v2019_3/Linux64bit+2.6-2.12-e17-prof/lib:/data4/NEXT/software/scisoft/gcc/v7_3_0/Linux64bit+2.6-2.12/lib64:$LD_LIBRARY_PATH
-else if [[ ${HOSTNAME} =~ "fnal" ]] ; then
+fi
+if [[ ${HOSTNAME} =~ "fnal" ]] ; then
          # set the gcc dir
          gcc_dir=v8_2_0
          
@@ -45,7 +46,3 @@ fi
 export CMAKE_MODULE_PATH=$ROOTSYS
 export CMAKE_PREFIX_PATH=$ROOTSYS
 export HIGHLAND_PACKAGE_HIERARCHY=pionAnalysis:baseAnalysis:highlandTools:psycheSelections:highlandIO:highlandCorrections:highlandUtils:LArSoftReader:highlandEventModel:highlandCore:psycheIO:psycheDUNEUtils:psycheUtils:psycheEventModel:psycheCore
-
-
-
-
