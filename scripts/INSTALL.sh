cd1c51b850c2b303f0b62737ef6f8d57f47b1246
Anselmo Cervera <acervera@fnal.gov>
2020-03-27 10:45:55 +0100

    do everyting from the base folder, without going inside build

--- scripts/INSTALL.sh
+++ scripts/INSTALL.sh
@@ -11,29 +11,26 @@ if [ -z "${found}" ]; then
    return
 fi
 
-# set environment variables needed by cmake
-source scripts/setup.sh
-
 # create a working directory
 if [ -d "build" ]; then
     echo "build folder already exists. You should delete it manually or source cleanup.sh "
     return
 fi
 
+# creates the build directory
 mkdir build
-cd build
 
-# create the Makefile
-cmake ..
+# set environment variables needed by cmake
+source scripts/setup.sh
+
+# create the Makefile inside the build folder
+cmake -S . -B build
 
 # compile  (equivalent to "make" in most systems)
-cmake --build .
+cmake --build build
 
 #install libraries and binaries in the appropriate folder  (equivalent to "make install" in most systems)
-cmake --install .
-
-# go to the initial folder
-cd ..
+cmake --install build
 
 # set environment variables that need to run cmake first
 source scripts/setup_cmake.sh
