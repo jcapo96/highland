bbc654fd3240e916b4a74f41cdd463e3df388a9d
Anselmo Cervera <acervera@fnal.gov>
2020-03-31 15:37:06 +0200

    remove pionAnalysis from highland library since pionAnalysis is now a separate project in git

--- src/highland2/pionAnalysis/app/RunPionAnalysis.cxx
+++
@@ -1,8 +0,0 @@
-#include "pionAnalysis.hxx"
-#include "AnalysisLoop.hxx"
-
-int main(int argc, char *argv[]){
-  pionAnalysis* ana = new pionAnalysis();
-  AnalysisLoop loop(ana, argc, argv); 
-  loop.Execute();
-}
