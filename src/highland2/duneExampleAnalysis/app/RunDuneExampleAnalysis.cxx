cf14ac316bc00a4b47f39e71474c4bc2a3dc8ad5
Anselmo Cervera <acervera@fnal.gov>
2021-09-02 13:36:47 +0200

    remove obsolete package

--- src/highland2/duneExampleAnalysis/app/RunDuneExampleAnalysis.cxx
+++
@@ -1,8 +0,0 @@
-#include "duneExampleAnalysis.hxx"
-#include "AnalysisLoop.hxx"
-
-int main(int argc, char *argv[]){
-  duneExampleAnalysis* ana = new duneExampleAnalysis();
-  AnalysisLoop loop(ana, argc, argv); 
-  loop.Execute();
-}
